5fd10d9c7aa99a912017d134a53b70d7b6993825
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2017-12-16 12:52:59 +0100

    Acelerómetro funcionando

--- android/PruebaAcelerometro/app/src/main/java/com/jaureguialzo/pruebaacelerometro/MainActivity.kt
+++ android/PruebaAcelerometro/app/src/main/java/com/jaureguialzo/pruebaacelerometro/MainActivity.kt
@@ -1,13 +1,26 @@
 package com.jaureguialzo.pruebaacelerometro
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_main.view.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
 
     val IP = "192.168.1.103"
+
+    // REF: Uso de sensores: https://developer.android.com/guide/topics/sensors/sensors_motion.html
+    var mSensorManager: SensorManager? = null
+    var mSensor: Sensor? = null
+
+    private val TAG = "MainActivity"
+
+    var n = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,23 +35,65 @@ class MainActivity : AppCompatActivity() {
         servidor.text = " "
 
         botonIniciar.setOnClickListener(){
-
+            mSensorManager!!.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
         }
 
         botonParar.setOnClickListener() {
+            mSensorManager!!.unregisterListener(this);
+
             actualizarPantalla()
 
             seekBarH.progress = 50  // 0 -> 100
             seekBarV.progress = 50
+
+            n = 0
         }
 
         enviar.setOnCheckedChangeListener(){ v, estado ->
             servidor.text = if ( estado == true ) "IP: ${IP}" else " "
         }
 
+        mSensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        mSensor = mSensorManager!!.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
     }
 
-    fun actualizarPantalla(x: Double? = null, y: Double? = null, z: Double? = null) {
+    override fun onPause() {
+        super.onPause()
+
+        // REF: No se para solo si cerramos la actividad: https://developer.android.com/reference/android/hardware/SensorManager.html
+        mSensorManager!!.unregisterListener(this);
+    }
+
+    // REF: Recibir eventos: https://developer.android.com/reference/android/hardware/SensorEventListener.html
+    override fun onAccuracyChanged(event: Sensor?, accuracy: Int) {
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+
+        val x = event!!.values[0];
+        val y = event!!.values[1];
+        val z = event!!.values[2];
+
+        actualizarPantalla(x,y,z)
+
+        val g = 9.81
+
+        val X = ((x/g*50)+50).toInt()
+        val Y = ((y/g*50)+50).toInt()
+
+        seekBarH.progress = X
+        seekBarV.progress = Y
+
+        if( enviar.isChecked ) {
+            Log.d(TAG,"Enviando${n}")
+
+            n += 1
+        }
+
+    }
+
+    fun actualizarPantalla(x: Float? = null, y: Float? = null, z: Float? = null) {
 
         if (x != null) {
             this.labelX.text = "x: %.3f".format(x)  // REF: Formato: https://stackoverflow.com/a/33492636/5136913
